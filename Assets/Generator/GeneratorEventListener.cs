54c919f140deca999a3e4c47f56caadf3555ae80
Ondatra Eating My Head <51259756+HasAzRus@users.noreply.github.com>
2022-12-11 01:29:32 +0500

    feat: add generator

---
+++ Assets/Generator/GeneratorEventListener.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class GeneratorEventListener : MonoBehaviour
+{
+	[SerializeField] private Generator _generator;
+	[SerializeField] private UnityEvent _events;
+
+	private void OnGeneratorLaunched()
+	{
+		_events.Invoke();
+	}
+
+	private void OnEnable()
+	{
+		_generator.Launched += OnGeneratorLaunched;
+	}
+
+	private void OnDisable()
+	{
+		_generator.Launched -= OnGeneratorLaunched;
+	}
+}
--- Assets/Interactive/Puller.cs
+++ Assets/Interactive/Puller.cs
@@ -3,6 +3,9 @@
 [RequireComponent(typeof(Rigidbody))]
 public class Puller : GrabInteractive
 {
+	private Bathyscaph _bathyscaph;
+	private bool _isDragging;
+
 	private Rigidbody _rigidbody;
 
 	private void Start()
@@ -10,19 +13,31 @@ public class Puller : GrabInteractive
 		_rigidbody = GetComponent<Rigidbody>();
 	}
 
+	private void OnDestroy()
+	{
+		if (_isDragging)
+		{
+			_bathyscaph.Drop();
+		}
+	}
+
 	protected override void OnInteract(Bathyscaph bathyscaph)
 	{
 		base.OnInteract(bathyscaph);
+
+		_bathyscaph = bathyscaph;
 
 		_rigidbody.useGravity = false;
 
 		bathyscaph.Drag(transform);
 
+		_isDragging = true;
 	}
 
 	protected override void OnStopInteraction(Bathyscaph bathyscaph)
 	{
 		bathyscaph.Drop();
 		_rigidbody.useGravity = true;
+		_isDragging = false;
 	}
 }
